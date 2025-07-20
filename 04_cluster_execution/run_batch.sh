bf8cd4a6d6be6a110562def08cda3980106abbf2
Javier Vela <javier.vela00@gmail.com>
2025-07-20 12:54:53 +0200

    Update README.md and run_batch.sh for improved clarity and configuration details

--- 04_cluster_execution/run_batch.sh
+++ 04_cluster_execution/run_batch.sh
@@ -10,14 +10,18 @@
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=jvela@ipe.csic.es
 
+# Load necessary modules
 module load cesga/2022 ollama/0.6.4 python/3.10.8
 
-export OLLAMA_HOST="127.0.0.1:11434"
+# Set up environment variables
+export OLLAMA_HOST="127.0.0.1:11433"
 export OLLAMA_TMPDIR=$TMPDIR
 export MAX_RETRIES=5
 
+# Run the Ollama server
 ollama serve >$LOG_DIR/ollama_server.log 2>&1 &
 
+# Wait for the Ollama server to start
 RETRY_COUNT=0
 while ! curl -s $OLLAMA_HOST | grep -q "Ollama is running"; do
     echo "Ollama server is not running. Retrying..."
@@ -29,6 +33,6 @@ while ! curl -s $OLLAMA_HOST | grep -q "Ollama is running"; do
     sleep 1
 done
 
+# Run the Python script
 cd $HOME/llm-information-extraction-workshop
-
 poetry run python 04_cluster_execution/01_test_interactive.py
